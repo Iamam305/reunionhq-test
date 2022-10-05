a3e68a18ffbefe5c069af8bf63f37740be9f6983
ajitesh <ajitesh314mishra@gmail.com>
2022-10-05 22:04:27 +0530

    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 
@@ -7,25 +7,9 @@ import { Outlet } from "react-router";
 import Footer from "./components/Footer";
 
 function App() {
-  // const [properties, setProperties] = useState([]);
-  // console.log(properties);
-  const params = {
-    locationExternalIDs: '5002,6020',
-    purpose: 'for-rent',
-    hitsPerPage: '25',
-    page: '0',
-    lang: 'en',
-    sort: 'city-level-score',
-    rentFrequency: 'monthly',
-    categoryExternalID: '4',
-    priceMax: '1000',
-    areaMax: '100'
-  };
 
-  useEffect(() => {
-    console.log(import.meta.env.KEY)
-    console.log(import.meta.env.VITE_API_KEY);
-  }, []);
+
+
   return (
     <div className="">
       <Navbar/>
